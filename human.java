72d33d5667f1fa289083e5962881b4b3ab88bb6d
elenk7 <elkokka07@gmail.com>
2026-05-02 12:17:53 +0300

    Update human.java

--- human.java
+++ human.java
@@ -3,18 +3,10 @@ public class Human{
     private String phone;
 
     Human(String name, String phone){
-=======
-class Human {
-    private String phone;
-    private String name;
-    
-    Human(String name, String phone) {
->>>>>>> b7406e70ce036357444474a10d95e4d5c42cfaf5
         this.name = name;
         this.phone = phone;
     }
 
-<<<<<<< HEAD
     public String getName(){
         return this.name;
     }
@@ -32,28 +24,6 @@ class Human {
     }
 
     public String toString(){
-        return "Name:" + this.name + "/nPhone" + this.phone;
-    }
-}}
-=======
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String toString() {
-        return "Name: " + name + ", Phone: " + phone;
+        return "Name: " + this.name + "/nPhone: " + this.phone;
     }
 }
->>>>>>> b7406e70ce036357444474a10d95e4d5c42cfaf5
