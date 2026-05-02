2612775ff385f3c7fcd44db958c724f3272750bb
elenk7 <elkokka07@gmail.com>
2026-05-02 12:11:26 +0300

    Update human.java

---
+++ human.java
@@ -0,0 +1,29 @@
+public class Human{
+    private String name;
+    private String phone;
+
+    Human(String name, String phone){
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getPhone(){
+        return this.phone;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+
+    public String toString(){
+        return "Name:" + this.name + "/nPhone" + this.phone;
+    }
+}
