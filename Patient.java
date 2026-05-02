2c81b4b8763d35fbc0cd4ee00fc56678e6dc6ad4
Efyyy <138901295+naomii8@users.noreply.github.com>
2026-05-02 12:39:53 +0300

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,9 +1,12 @@
 class Patient extends Human {
     private String email;
+    private static int idCounter = 1;
+    private int code;
 
     Patient(String name, String phone, String email) {
         super(name, phone);
         this.email = email;
+        this.code = idCounter++;
     }
 
     public String getEmail() {
@@ -13,9 +16,17 @@ class Patient extends Human {
     public void setEmail(String email) {
         this.email = email;
     }
+    
+    public int getCode() {
+        return this.code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
 
     public String toString() {
-        return super.toString() + "\nEmail:" + this.email;
+        return super.toString() + "\nEmail:" + this.email + "\nID:" + this.code;
     }
-    
+
 }
