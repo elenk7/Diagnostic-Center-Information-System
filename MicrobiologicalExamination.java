b8c8607c4560ea862e1a171df95d482cb8a57765
elenk7 <elkokka07@gmail.com>
2026-05-02 14:23:59 +0300

    Update MicrobiologicalExamination.java

--- MicrobiologicalExamination.java
+++ MicrobiologicalExamination.java
@@ -4,6 +4,8 @@ public class MicrobiologicalExamination extends Exam{
     private static final double costIncreaseRate = 0.2;
 
     MicrobiologicalExamination(String examName, String categoryName, int maxSlots, int doctorID, double cost, String sampleType){
+        /*Constructor for Microbiological Examination class that initializes the exam name, category name, maximum slots, doctor ID, cost, and sample type.
+         It also calls the constructor of the superclass Exam to initialize the common attributes of all exams.*/
         super(examName, categoryName, maxSlots, doctorID);
         this.examCost = cost;
         this.sampleType = sampleType;
