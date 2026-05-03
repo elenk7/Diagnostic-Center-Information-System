2686824368f5e3c3d3e00478ab721b0b88d9b755
Efyyy <138901295+naomii8@users.noreply.github.com>
2026-05-03 17:49:32 +0300

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -37,6 +37,6 @@ public class Doctor extends Human{
 
     @Override
     public String toString(){
-        return "DoctorID: " + this.code +"/n" + super.toString() + "/nSpecialty: " + this.specialty + "/nYears of Experience: " + this.experience;
+        return super.toString() + "\nDoctorID: " + this.code + "\nSpecialty: " + this.specialty + "\nYears of Experience: " + this.experience;
     }
 }
