cd786a7e8543deafdc1e1bb24539c3490936a8e8
Efyyy <138901295+naomii8@users.noreply.github.com>
2026-05-03 17:19:49 +0300

    Update ExamMenu.java

--- ExamMenu.java
+++ ExamMenu.java
@@ -1,1 +1,38 @@
-public class ExamMenu{}
+import java.util.HashMap;
+public class ExamMenu{
+    private HashMap<Integer, Exam> exams = new HashMap<>();
+
+    public void addExam(Exam exam){
+        exams.put(exam.getCode(), exam);
+    }
+
+    public void showExams(){
+        if (exams.isEmpty()){
+            System.out.println("No exams");
+        }else {
+            for (Exam exam: exams.values()){
+                System.out.println(exam);    
+            }     
+        }
+    }
+
+    public void examAppointments(int examID){
+        boolean found = false;
+        for (Exam exam: exams.values()){
+            if (exam.getCode() == examID){
+                for(Appointment appointment : appointments.values()){
+                   if (appointment.getExamID() = examID){
+                    System.out.println(appointment);
+                    found = true;
+                   }    
+                }    
+            }
+        }
+        if (!found){
+            System.out.println("No appointments for exam ID: " + examID);
+        }
+        
+    }
+
+    
+}
