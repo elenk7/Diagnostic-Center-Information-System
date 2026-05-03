215a1bbe3832e24d84518aca28ffec9e0284949f
Efyyy <138901295+naomii8@users.noreply.github.com>
2026-05-03 17:41:11 +0300

    Update PatientMenu.java

--- PatientMenu.java
+++ PatientMenu.java
@@ -1,53 +1,32 @@
 import java.util.HashMap;
-import java.util.Scanner;
 
 class PatientMenu {
     private HashMap<Integer, Patient> patients= new HashMap<>();
-    private Scanner scanner = new Scanner(System.in);
-
-    public void displayPatientMenu() {
-
-         private boolean back=true;
-
-         while (back) {
-            System.out.println("1. Add Patient");
-            System.out.println("2. View Patients");
-            System.out.println("3. View details of a specific patient");
-            System.out.println("4. Back to Main Menu");
-       
-            int choice = scanner.nextInt();
-            scanner.nextLine(); 
-
-            switch (choice) {
-                case 1:
-                    public void addPatient(){
-                        Patient patient = new Patient();
-                        patients.put(patient.getCode(), patient);
-                        System.out.println("Patient added successfully. Patient ID: " + patient.getCode());
-                    }
-                case 2:
-                    public void viewPatients(){
-                        if (patients.isEmpty()) {
-                            System.out.println("No patients found.");
-                        } else {
-                            for (Patient patient : patients.values()) {
-                                System.out.println(patient);
-                            }
-                        }
-                    }
-                case 3:
-                    public void findByCode(int code){ 
-                        Patient patient = patients.get(code);
-                        if (patient != null) {
-                            System.out.println(patient);
-                        } else {
-                            System.out.println("Patient not found.");
-                        }
-                    }
-                case 4:
-                    back = false;    
-            } 
+   
+    public void addPatient(){
+        patients.put(patient.getCode(), patient);
+    }
+               
+    public void viewPatients(){
+        if (patients.isEmpty()) {
+            System.out.println("No patients found.");
+        } else {
+            for (Patient patient : patients.values()) {
+                System.out.println(patient);
+            }
+        }
+    }
+                
+    public void findByCode(int code){ 
+        Patient patient = patients.get(code);
+        if (patient != null) {
+            for (Appointment appointment : appointments.values()){
+                if (appointment.getPatientID() == code) {
+                    System.out.println(appointment);
+                }
+            }
+        } else {
+             System.out.println("Patient not found.");
         }
     }                
-    
-}
+} 
