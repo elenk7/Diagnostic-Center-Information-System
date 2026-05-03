03015a63e8c450d01b246243b2435c0bee711fe3
Efyyy <138901295+naomii8@users.noreply.github.com>
2026-05-03 17:42:05 +0300

    Update Exam.java

--- Exam.java
+++ Exam.java
@@ -1,63 +1,72 @@
 abstract class Exam{
-    private static int idCounter = 1;
-    private int code;
+    private String code;
     private String examName;
     private String categoryName;
+    private String examSpecialty;
     private int maxSlots;
+    private double cost;
     private int doctorID;
 
-    Exam(String examName, String categoryName, int maxSlots, int doctorID){
-        this.code = idCounter++;
+
+    abstract double getCost();
+
+    Exam(String code, String examName, String categoryName, String examSpecialty, String maxSlots, double cost, int doctorID){
+        this.code = code;
         this.examName = examName;
         this.categoryName = categoryName;
+        this.examSpecialty = examSpecialty;
         this.maxSlots = maxSlots;
+        this.cost = cost;
         this.doctorID = doctorID;
     }
 
-    public String getExamName(){
-        return this.examName;
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public void setExamName(String examName) {
+        this.examName = examName;
+    }
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+    public void setExamSpecialty(String examSpecialty) {
+        this.examSpecialty = examSpecialty;
+    }
+    public void setMaxSlots(String maxSlots) {
+        this.maxSlots = maxSlots;
+    }
+   
+    public void setDoctorID(int doctorID) {
+        this.doctorID = doctorID;
     }
 
-    public String getCategoryName(){
-        return this.categoryName;
-    }
-
-    public int getMaxSlots(){
-        return this.maxSlots;
-    }
-
-    public int getCode(){
+    public String getCode() {
         return this.code;
     }
 
-    public int getDoctorID(){
+    public String getExamName() {
+        return this.examName;
+    }
+
+    public String getCategoryName() {
+        return this.categoryName;
+    }
+
+    public String getExamSpecialty() {
+        return this.examSpecialty;
+    }
+
+    public int getMaxSlots() {
+        return this.maxSlots;
+    }
+
+    public int getDoctorID() {
         return this.doctorID;
     }
-    
-    public void setCode(int code){
-        this.code = code;
-    }
-
-    public void setExamName(String examName){
-        this.examName = examName;
-    }
-
-    public void setCategoryName(String categoryName){
-        this.categoryName = categoryName;
-    }
-
-    public void setDoctorID(int doctorID){
-        this.doctorID = doctorID;
-    }
-
-    public void setMaxSlots(int maxSlots){
-        this.maxSlots = maxSlots;
-    }
-
-    public abstract double getCost();
 
     @Override
     public String toString(){
-        return "AppointmentID: " + this.code + "/nExam Name: " + this.examName + "/nCategory Name: " + this.categoryName + "/nMax Slots per Day: " + this.maxSlots + "/nDoctorID: " + this.doctorID;
+        return "AppointmentID: " + this.code + "\nExam Name: " + this.examName + "\nCategory Name: " + this.categoryName + "\nMax Slots per Day: " + this.maxSlots + "\nDoctorID: " + this.doctorID;
     }
+
 }
