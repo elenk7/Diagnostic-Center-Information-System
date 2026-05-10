0fc6c1bbddbec5c7057a530e7bd2d0af1f1239c2
Efyyy <138901295+naomii8@users.noreply.github.com>
2026-05-10 13:12:28 +0300

    Update ImagingExamination.java

--- ImagingExamination.java
+++ ImagingExamination.java
@@ -1,15 +1,19 @@
+import java.util.HashMap;
 class ImagingExamination extends Exam {
     private String machineType;
     private double cost;
     private double examCost;
     private static final double costIncreaseRate = 0.10;
+    private boolean fastResults;
     
 
-    ImagingExamination(String code, String examName, String categoryName, String examSpecialty, int maxSlots, double cost, int doctorID, String machineType) {
+    ImagingExamination(int code, String examName, String categoryName, String examSpecialty, int maxSlots, double cost, int doctorID, String machineType) {
         super(code, examName, categoryName, examSpecialty, maxSlots, cost, doctorID);
         this.machineType = machineType;
         this.cost = cost;
     }
+
+    
 
     public String getMachineType() {
         return this.machineType;
@@ -19,7 +23,13 @@ class ImagingExamination extends Exam {
         this.machineType = machineType;
     }
 
-    public double getCost(){
+
+    public double getCost(HashMap<Integer, Appointment> appointments){
+        for(Appointment appointment : appointments.values()){
+            if (this.getCode() == appointment.getExamID()){
+                fastResults = appointment.getFastResults();
+            }
+        }
         if (fastResults){
             examCost = cost + (cost * costIncreaseRate);
         }else{
@@ -29,6 +39,6 @@ class ImagingExamination extends Exam {
     } 
     
     public String toString(){
-        return super.toString() + "/nMachine Type: " + this.machineType + "/nCost: " + this.examCost;
+        return super.toString() + "\nMachine Type: " + this.machineType + "\nCost: " + this.examCost;
     }
 }
