c7a512872ec5416efaa9c22d81437e5b13cb56ca
Efyyy <138901295+naomii8@users.noreply.github.com>
2026-05-15 15:43:41 +0300

    Update Statistics.java

--- Statistics.java
+++ Statistics.java
@@ -1,81 +1,64 @@
 import java.util.HashMap;
 public class Statistics{
-        public double revenuePatient(int patientID, HashMap<Integer, Appointment> appointments, HashMap<Integer, Exam> exams){
-                double sum = 0;
-                for(Appointment appointment : appointments.values()){
-                        if (appointment.getPatientID() == patientID){
-                                for (Exam exam:exams.values()){
-                                        if(appointment.getExamID() == exam.getCode()){
-                                                sum = sum + exam.getCost(appointments);
+        public void revenuePatient(HashMap<Integer, Appointment> appointments, HashMap<Integer, Exam> exams, HashMap<Integer, Patient> patients){
+                double totalRevenue = 0;
+                for ( Patient patient : patients.values()){
+                        double sum = 0;
+                        
+                        for(Appointment appointment : appointments.values()){
+                                if (appointment.getPatientID() == patient.getCode() && !appointment.getDeleted()){
+                                        for (Exam exam:exams.values()){
+                                                if(appointment.getExamID() == exam.getCode()){
+                                                        System.out.println(appointment);
+                                                        System.out.println(exam.getCost(appointments));
+                                                        sum = sum + exam.getCost(appointments);
+                                                }
                                         }
                                 }
+                        
                         }
+                        System.out.println("Patient: " + patient.getName() + " Revenue: " + sum);
+                        totalRevenue = totalRevenue + sum;
                 }
-
-                return sum;
+                System.out.println("Total Revenue: " + totalRevenue + "\n");
+        }
+        
+        public void revenueAppointments(HashMap<Integer, Appointment> appointments, HashMap<Integer, Exam> exams){
+                double total=0;
+                for(Appointment appointment : appointments.values()){
+                        double sum=0;
+                        for (Exam exam:exams.values()){
+                                if(appointment.getExamID() == exam.getCode() && !appointment.getDeleted()){
+                                        for (Exam exam1:exams.values()){
+                                                if(appointment.getExamID() == exam1.getCode()){
+                                                        sum = sum + exam1.getCost(appointments);
+                                                }
+                                        }
+                                }
+                        } 
+                        System.out.println(appointment + " Revenue: " + sum);
+                        total = total + sum;
+                             
+                }
                 
-
+                System.out.println("Total Revenue: " + total);
         }
-        public double revenueAllPatients(HashMap<Integer, Appointment> appointments, HashMap<Integer, Exam> exams, HashMap<Integer, Patient> patients){
+
+        public void revenueExam(HashMap<Integer, Appointment> appointments, HashMap<Integer, Exam> exams){
                 double sum = 0;
-                for (Patient patient : patients.values()){
-                        sum = sum + revenuePatient(patient.getCode(), appointments, exams);
-                }
-                return sum;
-
-        }
-        public double revenueAppointments(int examID, HashMap<Integer, Appointment> appointments, HashMap<Integer, Exam> exams){
-                double sum = 0;
-                Exam exam = exams.get(examID);
-                for(Appointment appointment : appointments.values()){              
-                        if (appointment.getExamID() == examID){
-                                sum = sum + exam.getCost(appointments);
-                        }
-                }
-                return sum;
-        }
-
-        public double revenueAllAppointments(HashMap<Integer, Appointment> appointments, HashMap<Integer, Exam> exams){
-                double sum = 0;
-                for (Exam exam : exams.values()){
-                        sum = sum + revenueAppointments(exam.getCode(), appointments, exams);
-                }
-                return sum;
-        }
-
-        public double revenueExam(String category, HashMap<Integer, Appointment> appointments, HashMap<Integer, Exam> exams){
-                double sum = 0;
-                if(category.equals("Imaging")){
-                        for (Exam exam:exams.values()){
-                                if (exam.getCategoryName().equals(category)){
-                                        sum = sum + exam.getCost(appointments);
-                                }
-                        }
-                        return sum;
-                }else if(category.equals("Microbiological")){
-                        for (Exam exam:exams.values()){
-                                if (exam.getCategoryName().equals(category)){
-                                        sum = sum + exam.getCost(appointments);
-                                }
-                        }
-                        return sum;
-                }else if(category.equals("Specialized")){
-                        for (Exam exam:exams.values()){
-                                if (exam.getCategoryName().equals(category)){
-                                        sum = sum + exam.getCost(appointments);
-                                }
-                        }
-                        return sum;
-                }
-                return 0;
-        }
-
-        public double revenueAllExams( HashMap<Integer, Appointment> appointments, HashMap<Integer, Exam> exams){
-                double sum=0;
                 for (Exam exam:exams.values()){
-                        sum = sum + exam.getCost(appointments);
+                        double sumExam = 0;
+                        for (Appointment appointment:appointments.values()){
+                                if(appointment.getExamID() == exam.getCode() && !appointment.getDeleted()){
+                                        System.out.println(appointment);
+                                        System.out.println(exam.getCost(appointments));
+                                        sumExam = sumExam + exam.getCost(appointments);
+                                }
+                        }
+                        System.out.println("Exam: " + exam.getExamName() + " Revenue: " + sumExam);
+                        sum = sum + sumExam;
                 }
-                return sum;
+                System.out.println("Total Revenue: " + sum);
         }
 
         
