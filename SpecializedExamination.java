ac0181540c066805466be361991e8dd0f2db5577
Efyyy <138901295+naomii8@users.noreply.github.com>
2026-05-15 15:43:36 +0300

    Update SpecializedExamination.java

--- SpecializedExamination.java
+++ SpecializedExamination.java
@@ -2,13 +2,13 @@ import java.util.HashMap;
 class SpecializedExamination extends Exam{
     private String examSpecialty;
     private double cost;
-    private double examCost;
     private static final double costIncreaseRate = 0.30;
     private boolean fastResults;
 
 
     SpecializedExamination(String examName, String categoryName, String examSpecialty, int maxSlots, double cost, int doctorID){
         super(examName, categoryName, examSpecialty, maxSlots, cost, doctorID);
+        this.examSpecialty = examSpecialty;
         this.cost = cost;
     }
 
@@ -23,20 +23,20 @@ class SpecializedExamination extends Exam{
     @Override
     public double getCost(HashMap<Integer, Appointment> appointments){
         for(Appointment appointment : appointments.values()){
-            if (this.getCode() == appointment.getExamID()){
+            if (this.getCode() == appointment.getExamID()){  //Checks if the current exam's code matches the exam ID in the appointment. If it does, it retrieves the fast results option from the appointment and stores it in the fastResults variable.
                 fastResults = appointment.getFastResults();
             }
         }
-        if (fastResults){
-            examCost = cost + (cost * costIncreaseRate);
+        if (fastResults){  //If the fast results option is true, it calculates the exam cost by adding a percentage increase to the base cost and returns the updated exam cost.
+            cost = cost + (cost * costIncreaseRate);
         }else{
-            examCost = cost;
+            cost = cost;
         }   
-        return examCost;
+        return cost;
     }
 
     public String toString(){
-        return super.toString() + "\nExam Specialty: " + this.examSpecialty + "\nCost: " + this.examCost;
+        return super.toString() + " | Exam Specialty: " + this.examSpecialty + " | Cost: " + this.cost;
     }
     
 }
