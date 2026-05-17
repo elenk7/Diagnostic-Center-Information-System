d2a591201820361375585a973e3d4de64ce7d857
elenk7 <elkokka07@gmail.com>
2026-05-17 14:52:45 +0300

    Update Statistics.java

--- Statistics.java
+++ Statistics.java
@@ -7,12 +7,16 @@ public class Statistics{
                         double sum = 0;
                         
                         for(Appointment appointment : appointments.values()){  //we loop through all the appointments to check if the patient has any appointments
-                                if (appointment.getPatientID() == patient.getCode() && !appointment.getDeleted()){  //if the patientID of the appointment matches the code of the patient and the appointment is not deleted we get the cost of the exam and add it to the sum of the patient
+                                if (appointment.getPatientID() == patient.getCode() && !appointment.getDeleted()){  
+                                /*if the patientID of the appointment matches the code of the patient and the appointment 
+                                is not deleted we get the cost of the exam and add it to the sum of the patient*/
                                         for (Exam exam:exams.values()){  
-                                                if(appointment.getExamID() == exam.getCode()){  //we check if the examID of the appointment matches the code of the exam, if it does we get the cost of the exam to print it later and add it to the sum of the patient
+                                                if(appointment.getExamID() == exam.getCode()){  
+                                                /*we check if the examID of the appointment matches the code of the exam, 
+                                                if it does we get the cost of the exam to print it later and add it to the sum of the patient*/
                                                         System.out.println(appointment);  
                                                         System.out.println(exam.getCost(appointments)); //we print the appointment and the cost of the exam for the patient
-                                                        sum = sum + exam.getCost(appointments);
+                                                        sum = sum + exam.getExamCost();
                                                 }
                                         }
                                 }
@@ -32,7 +36,7 @@ public class Statistics{
                                 if(appointment.getExamID() == exam.getCode() && !appointment.getDeleted()){  
                                         System.out.println(appointment);
                                         System.out.println(exam.getCost(appointments)); //we print the appointment and the cost of the exam
-                                        sumExam = sumExam + exam.getCost(appointments);
+                                        sumExam = sumExam + exam.getExamCost(); //we add the cost of the exam to the sum of the exam
                                 }
                         }
                         sum = sum + sumExam;
@@ -49,7 +53,7 @@ public class Statistics{
                         if (appointment.getExamID() == exam.getCode() && !appointment.getDeleted()) {
                             System.out.println(appointment);
                             System.out.println(exam.getCost(appointments));
-                            sumCategory = sumCategory + exam.getCost(appointments);
+                            sumCategory = sumCategory + exam.getExamCost();
                         }
                     }
                     categoryRevenue.put(exam.getCategoryName(), categoryRevenue.getOrDefault(exam.getCategoryName(), 0.0) + sumCategory);  //we add the revenue of the exam to the category, if the category does not exist we put 0 as default value
